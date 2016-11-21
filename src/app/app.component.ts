b00b056124a1fe3ddbdc542fbc82b6d66fdea6f2
tauqeer <tauqeer.shakir@yahoo.com>
2016-11-22 00:08:35 +0500

    Builtin directive angular 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  // title = 'app works!';
+
+  color: string;
+  font: number;
+  isBordered: boolean;
+  cities: string[];
+
+  constructor(){
+    this.isBordered = true;
+    this.cities = ['Karachi', 'Lahore', 'Multan', 'Peshawar', 'Quetta'];
+  }
+
+  apply(color: string, font: number){
+
+    console.log('font ', font);
+    console.log('type of font ', typeof font);
+
+    this.color = color;
+    this.font = font;
+
+  }
+  
+  selectCity(i: number): void{
+    
+    console.log('index ', i);
+    
+  }
+
+
+
 }
